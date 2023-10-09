28baecdd67729b38cc7ae0f46cab1b906f9449fe
jacksonhmg <jacksonmowattgok@gmail.com>
2023-10-09 10:15:22 +0800

    using streamlit to add pet type and colour type options

---
+++ main.py
@@ -0,0 +1,18 @@
+import langchain_helper as lch
+import streamlit as st
+
+st.title("Pet Name Generator")
+
+animal_type = st.sidebar.selectbox("What is your pet?", ("Cat", "Dog", "Cow", "Hamster"))
+
+if animal_type == "Cat":
+    pet_colour = st.sidebar.text_area("What is your cat's colour?", max_chars=15)
+
+if animal_type == "Dog":
+    pet_colour = st.sidebar.text_area("What is your dog's colour?", max_chars=15)
+
+if animal_type == "Cow":
+    pet_colour = st.sidebar.text_area("What is your cow's colour?", max_chars=15)
+
+if animal_type == "Hamster":
+    pet_colour = st.sidebar.text_area("What is your Hamster's colour?", max_chars=15)
