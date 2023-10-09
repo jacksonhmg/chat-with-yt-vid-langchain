78ae87056c013a66c23ec5b25b9b3bd80e870940
jacksonhmg <jacksonmowattgok@gmail.com>
2023-10-09 21:28:44 +0800

    youtube video's chatbot setup

--- langchain_helper.py
+++ langchain_helper.py
@@ -1,43 +1,26 @@
+from langchain.document_loaders import YoutubeLoader
+from langchain.text_splitter import RecursiveCharacterTextSplitter
 from langchain.llms import OpenAI
-from langchain.prompts import PromptTemplate
+from langchain.embeddings.openai import OpenAIEmbeddings
+from langchain import PromptTemplate
 from langchain.chains import LLMChain
-from langchain.agents import load_tools
-from langchain.agents import initialize_agent
-from langchain.agents import AgentType
-
+from langchain.vectorstores import FAISS
 from dotenv import load_dotenv
 
 load_dotenv()
 
-def generate_pet_names(animal_type, pet_colour):
-    llm = OpenAI(temperature=0.7)
+embeddings = OpenAIEmbeddings()
 
-    prompt_template_name= PromptTemplate(
-        input_variables=['animal_type', 'pet_colour'],
-        template="I have a pet {animal_type} and I want a cool name for it, it is {pet_colour} in colour. Suggest me five cool names for my {animal_type}"
-    )
+video_url = "https://www.youtube.com/watch?v=MVYrJJNdrEg&t=1577s&ab_channel=LexFridman"
 
-    name_chain = LLMChain(llm=llm, prompt=prompt_template_name, output_key="pet_name")
+def create_vector_db_from_youtube_url(video_url: str) -> FAISS:
+    loader = YoutubeLoader.from_youtube_url(video_url)
+    transcript = loader.load()
 
-    response = name_chain({'animal_type': animal_type, 'pet_colour': pet_colour})
-    return response
+    text_splitter = RecursiveCharacterTextSplitter(chunk_size=1000, chunk_overlap=100)
+    docs = text_splitter.split_documents(transcript)
 
-def langchain_agent():
-    llm = OpenAI(temperature=0.5)
+    db = FAISS.from_documents(docs, embeddings)
+    return db
 
-    tools = load_tools(["wikipedia", "llm-math"], llm = llm)
-
-    agent = initialize_agent(
-        agent = AgentType.ZERO_SHOT_REACT_DESCRIPTION,
-        llm = llm,
-        tools = tools,
-        verbose = True
-    )
-
-    result = agent.run(
-        "What is the average age of a dog? Multiply the age by 3"
-    )
-
-if __name__ == "__main__":
-    langchain_agent()
-    # print(generate_pet_names("cat", "orange"))
+create_vector_db_from_youtube_url(video_url)
