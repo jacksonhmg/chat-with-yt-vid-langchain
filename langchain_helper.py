6373ca1a0cd0f86f991d78378a142fee2867fca8
jacksonhmg <jacksonmowattgok@gmail.com>
2023-10-09 11:08:18 +0800

    using langchain agent. its inconsistent though

--- langchain_helper.py
+++ langchain_helper.py
@@ -1,6 +1,10 @@
 from langchain.llms import OpenAI
 from langchain.prompts import PromptTemplate
 from langchain.chains import LLMChain
+from langchain.agents import load_tools
+from langchain.agents import initialize_agent
+from langchain.agents import AgentType
+
 from dotenv import load_dotenv
 
 load_dotenv()
@@ -18,5 +22,22 @@ def generate_pet_names(animal_type, pet_colour):
     response = name_chain({'animal_type': animal_type, 'pet_colour': pet_colour})
     return response
 
+def langchain_agent():
+    llm = OpenAI(temperature=0.5)
+
+    tools = load_tools(["wikipedia", "llm-math"], llm = llm)
+
+    agent = initialize_agent(
+        agent = AgentType.ZERO_SHOT_REACT_DESCRIPTION,
+        llm = llm,
+        tools = tools,
+        verbose = True
+    )
+
+    result = agent.run(
+        "What is the average age of a dog? Multiply the age by 3"
+    )
+
 if __name__ == "__main__":
-    print(generate_pet_names("cat", "orange"))
+    langchain_agent()
+    # print(generate_pet_names("cat", "orange"))
