ad8e9ed0c7b0c5a6d38115856a3e6149449a807f
jacksonhmg <jacksonmowattgok@gmail.com>
2023-10-09 22:29:45 +0800

    enter api key in form

--- main.py
+++ main.py
@@ -6,6 +6,11 @@ st.title("Youtube Assistant")
 
 with st.sidebar:
     with st.form(key='my_form'):
+        api_key = st.sidebar.text_input(
+            label="What is your OpenAI API key?",
+            max_chars=200,
+            key="api_key",
+        )
         youtube_url = st.sidebar.text_area(
             label="What is the Youtube video url?",
             max_chars=200,
@@ -18,9 +23,9 @@ with st.sidebar:
 
         submit_button = st.form_submit_button(label='Submit')
 
-if query and youtube_url:
+if query and youtube_url and api_key:
     db = lch.create_vector_db_from_youtube_url(youtube_url)
-    response = lch.get_response_from_query(db, query)
+    response = lch.get_response_from_query(db, query, api_key)
     st.subheader("Answer:")
     st.text(textwrap.fill(response, 80))
         
